cbbc128b82c4096dd2be77d33d4ae1ff20074031
Leandro Carlos <leonpcsn@gmail.com>
2021-03-17 21:53:36 -0300

    Aula 01 finalizada

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Olá Mundo!');
+alert('Ainda não carregou a pagina toda')
+
+window.onload = function(){
+  alert('Agora carregou a pagina toda');
+  console.log('Agora eu carreguei a pagina completa!');
+}
